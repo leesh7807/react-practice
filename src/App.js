c2d1cdfac1db681670670e803a90bdc77f1aa084
leesh <leesh7807@gmail.com>
2024-08-02 16:16:35 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let post = "강남 우동 맛집";
+  let [log, setLogo] = useState('ReactBlog');
+  let [title, setTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [like, setLike] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <h4>{ log }</h4>
+      </div>
+      <div className='list'>
+        <h4>{ title[0] } <span onClick={() => { setLike(like + 1); } }>👍</span> { like } </h4>
+        <p>2월 18일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ title[1] }</h4>
+        <p>2월 18일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ title[2] }</h4>
+        <p>2월 18일 발행</p>
+      </div>
+      
+      <Modal/>
     </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className='modal'>
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
   );
 }
 
