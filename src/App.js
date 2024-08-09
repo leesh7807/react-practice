88a0765164c460304c58aa81e23fec049ac06e59
leesh <leesh7807@gmail.com>
2024-08-09 21:23:27 +0900

    'practice'

--- src/App.js
+++ src/App.js
@@ -1,44 +1,91 @@
+import { useMemo, useRef, useState, useCallback } from 'react';
 import './App.css';
-import { useState } from 'react';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
+
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는중...');
+  return users.filter(user => user.active).length;
+}
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+  const { username, email } = inputs;
+  const onChange = useCallback(e => {
+      const { name, value } = e.target;
+      setInputs(inputs => ({
+        ...inputs,
+        [name]: value
+      }));
+    },
+    []
+  );
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: true
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+      active: false
+    }
+  ]);
 
-  let post = "강남 우동 맛집";
-  let [log, setLogo] = useState('ReactBlog');
-  let [title, setTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
-  let [like, setLike] = useState(0);
-
+  const nextId = useRef(4);
+  const onCreate = useCallback(() => {
+      const user = {
+        id: nextId.current,
+        username,
+        email
+      };
+      setUsers(users => ([...users, user]))
+      setInputs({
+        username: '',
+        email: ''
+      });
+      nextId.current += 1;
+    },
+    [username, email]
+  );
+  const onRemove = useCallback((id) => {
+      setUsers(users => users.filter(user => user.id !== id));
+    },
+    []
+  );
+  const onToggle = useCallback((id) => {
+      setUsers(users => 
+        users.map(user => 
+          user.id === id ? { ...user, active: !user.active } : user
+        )
+      );
+    },
+    []
+  );
+  const count = useMemo(() => countActiveUsers(users), [users]);
   return (
-    <div className="App">
-      <div className='black-nav'>
-        <h4>{ log }</h4>
-      </div>
-      <div className='list'>
-        <h4>{ title[0] } <span onClick={() => { setLike(like + 1); } }>👍</span> { like } </h4>
-        <p>2월 18일 발행</p>
-      </div>
-      <div className='list'>
-        <h4>{ title[1] }</h4>
-        <p>2월 18일 발행</p>
-      </div>
-      <div className='list'>
-        <h4>{ title[2] }</h4>
-        <p>2월 18일 발행</p>
-      </div>
-      
-      <Modal/>
-    </div>
+    <>
+      <CreateUser 
+      username={username}
+      email={email}
+      onChange={onChange}
+      onCreate={onCreate}
+      />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+      <div>활성사용자 수 : {count}</div>
+    </>
   );
 }
-
-function Modal() {
-  return (
-    <div className='modal'>
-        <h4>제목</h4>
-        <p>날짜</p>
-        <p>상세내용</p>
-      </div>
-  );
-}
-
 export default App;
